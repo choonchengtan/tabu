6331e854a9088a7701bbba0c56de0fed13455373
cctan <chooncheng.tan@student.usm.my>
2016-12-14 22:26:24 +0800

    Add sequential flag in compile.sh

--- compile.sh
+++ compile.sh
@@ -1,20 +1,27 @@
+#python2.7 main.py -s ./tspfiles/ulysses16.tsp
 #python2.7 main.py -o ./tspfiles/ulysses16.tsp
 #python2.7 main.py -g ./tspfiles/ulysses16.tsp
 
+#python2.7 main.py -s ./tspfiles/att48.tsp
 #python2.7 main.py -o ./tspfiles/att48.tsp
 #python2.7 main.py -g ./tspfiles/att48.tsp
 
+#python2.7 main.py -s ./tspfiles/lin105.tsp
 #python2.7 main.py -o ./tspfiles/lin105.tsp
 #python2.7 main.py -g ./tspfiles/lin105.tsp
 
+#python2.7 main.py -s ./tspfiles/u574.tsp
 #python2.7 main.py -o ./tspfiles/u574.tsp
 #python2.7 main.py -g ./tspfiles/u574.tsp
 
+#python2.7 main.py -s ./tspfiles/u1060.tsp
 #python2.7 main.py -o ./tspfiles/u1060.tsp
 #python2.7 main.py -g ./tspfiles/u1060.tsp
 
+#python2.7 main.py -s ./tspfiles/fnl4461.tsp
 #python2.7 main.py -o ./tspfiles/fnl4461.tsp
 #python2.7 main.py -g ./tspfiles/fnl4461.tsp
 
+#python2.7 main.py -s ./tspfiles/usa13509.tsp
 #python2.7 main.py -o ./tspfiles/usa13509.tsp
 #python2.7 main.py -g ./tspfiles/usa13509.tsp
